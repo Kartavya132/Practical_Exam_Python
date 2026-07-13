759a70c12434dca1dec99f426d427c326a7f0718
Kartavya Patel <kartavyapatel630@gmail.com>
2026-07-13 09:58:02 +0530

    Adding the filtering system

--- fitness_tracker.py
+++ fitness_tracker.py
@@ -23,6 +23,7 @@ class FitnessTracker:
                 calories = int(input("Enter the calories : "))
             except ValueError:
                 print("Enter proper input")
+                continue
             else:
                 self.log_activity(name_act, duration, calories)
                 break
@@ -39,7 +40,49 @@ class FitnessTracker:
         print("Activity added successfully.")
 
     def calculate_metrics(self):
-        pass
+        print(f"The Total calories are :- {self.__data['Calories Burned'].sum()}")
+        print(f"The Average Duration are :- {self.__data['Duration (Minutes)'].mean()}")
+        print("The activity Frequency are :-")
+        print(self.__data["Activity Type"].value_counts())
+
+    def filter_activities(self, condition):
+        if condition[0] == "Activity Type":
+            return self.__data[
+                self.__data[condition[0]].str.lower() == condition[1].lower()
+            ]
+        elif condition[0] == "Date":
+            return self.__data[self.__data[condition[0]] == condition[1]]
+        elif condition[0] == "Duration (Minutes)":
+            return self.__data[self.__data[condition[0]] >= int(condition[1])]
+        elif condition[0] == "Calories Burned":
+            return self.__data[self.__data[condition[0]] >= int(condition[1])]
+        else:
+            print("Invalid column")
+            return None
+
+    def filteration(self):
+        print("\nSimple Filtering")
+        print("Choose a filter:")
+        print("1. Activity Type")
+        print("2. Duration (Minutes)")
+        print("3. Calories Burned")
+        print("4. Date")
+
+        choice = int(input("Enter your choice: "))
+        columns = ["Activity Type", "Duration(Minutes)", "Calories Burned", "Date"]
+
+        if columns[choice - 1] in self.__data.columns:
+            column = self.__data.columns[choice]
+            value = input(f"Enter the value for {column}: ")
+            condition = [column, value]
+
+            result = self.filter_activities(condition)
+            if result is not None:
+                print(result)
+            else:
+                print("There is no data as per condition")
+        else:
+            print("Invalid Choice")
 
     def __del__(self):
         del self.__data
@@ -66,8 +109,9 @@ def show_menu():
     print(" 2. Add new row")
     print(" 3. Calories Analyser")
     print(" 4. Data Filtering")
-    print(" 5. Data Visualization")
-    print(" 6. Exit")
+    print(" 5. Summary and other important")
+    print(" 6. Data Visualization")
+    print(" 7. Exit")
     print("-------------------------")
     return input("Enter you choice : ")
 
@@ -86,12 +130,14 @@ def main():
             case "2":
                 fitness_tracker.add_row()
             case "3":
-                pass
+                fitness_tracker.calculate_metrics()
             case "4":
-                pass
+                fitness_tracker.filteration()
             case "5":
                 pass
             case "6":
+                pass
+            case "7":
                 del fitness_tracker
                 return
             case _:
